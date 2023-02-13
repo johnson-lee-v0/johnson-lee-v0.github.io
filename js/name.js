2a8b79d2f0f505ac72f232463a8f61728f11f6a8
Johnson <johnson.lee@uwaterloo.ca>
2023-02-12 21:49:26 -0500

    Intro name and spacing

---
+++ js/name.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+    var text = document.getElementById("animate-name");
+    text.style.opacity = 0;
+
+    var isInViewport = function(element) {
+      var rect = element.getBoundingClientRect();
+      var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+      return rect.top >= 0 && rect.bottom <= windowHeight;
+    };
+
+    var animationInterval;
+    var handleVisibilityChange = function() {
+      if (isInViewport(text)) {
+        var opacity = 0;
+        animationInterval = setInterval(function() {
+          opacity += 0.1;
+          text.style.opacity = opacity;
+          if (opacity >= 1) {
+            clearInterval(animationInterval);
+          }
+        }, 200);
+      } else {
+        clearInterval(animationInterval);
+        text.style.opacity = 0;
+      }
+    };
+
+    window.addEventListener("scroll", handleVisibilityChange);
+    handleVisibilityChange();
+  };
