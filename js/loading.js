de6b3b56a94f80d884a7166542242dcad4fe2089
Johnson Lee <89353467+johnson-lee-v0@users.noreply.github.com>
2023-05-25 21:27:18 -0400

    Experience-First Time Animation + Reorder

--- js/loading.js
+++ js/loading.js
@@ -1,1 +1,27 @@
-/Work in Progress/ 
+window.onload = function() {
+    var events = document.querySelectorAll(".timeline-event");
+  
+    var isInViewport = function(element) {
+      var rect = element.getBoundingClientRect();
+      var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+      return rect.top >= 0 && rect.bottom <= windowHeight;
+    };
+  
+    var handleVisibilityChange = function() {
+      events.forEach(function(event) {
+        if (isInViewport(event)) {
+          event.classList.add("animate");
+        }
+      });
+    };
+  
+    var throttleTimeout;
+    var handleScroll = function() {
+      clearTimeout(throttleTimeout);
+      throttleTimeout = setTimeout(handleVisibilityChange, 50);
+    };
+  
+    window.addEventListener("scroll", handleScroll);
+    handleVisibilityChange();
+  };
+  
