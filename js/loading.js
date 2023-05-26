34136e28e6cb05283223b9e6fc9c99f4f593c568
Johnson Lee <89353467+johnson-lee-v0@users.noreply.github.com>
2023-05-25 21:38:13 -0400

    Animation Fix

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,9 @@
 window.onload = function() {
-    var events = document.querySelectorAll(".timeline-event");
+    var text = document.getElementById("animate-name");
+    text.style.opacity = 0;
+  
+    var timelineEvents = document.querySelectorAll(".timeline-event");
+    var lastVisibleIndex = -1;
   
     var isInViewport = function(element) {
       var rect = element.getBoundingClientRect();
@@ -7,18 +11,45 @@ window.onload = function() {
       return rect.top >= 0 && rect.bottom <= windowHeight;
     };
   
-    var handleVisibilityChange = function() {
-      events.forEach(function(event) {
-        if (isInViewport(event)) {
+    var animateText = function() {
+      if (isInViewport(text)) {
+        var opacity = 0;
+        var animationInterval = setInterval(function() {
+          opacity += 0.1;
+          text.style.opacity = opacity;
+          if (opacity >= 1) {
+            clearInterval(animationInterval);
+          }
+        }, 150);
+      } else {
+        text.style.opacity = 0;
+      }
+    };
+  
+    var animateTimelineEvents = function() {
+      for (var i = 0; i < timelineEvents.length; i++) {
+        if (isInViewport(timelineEvents[i])) {
+          lastVisibleIndex = i;
+        }
+      }
+  
+      for (var j = 0; j <= lastVisibleIndex; j++) {
+        var event = timelineEvents[j];
+        if (!event.classList.contains("animate")) {
           event.classList.add("animate");
         }
-      });
+      }
+    };
+  
+    var handleVisibilityChange = function() {
+      animateText();
+      animateTimelineEvents();
     };
   
     var throttleTimeout;
     var handleScroll = function() {
       clearTimeout(throttleTimeout);
-      throttleTimeout = setTimeout(handleVisibilityChange, 50);
+      throttleTimeout = setTimeout(handleVisibilityChange, 200);
     };
   
     window.addEventListener("scroll", handleScroll);
