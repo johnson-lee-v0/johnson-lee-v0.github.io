3f403367807e8febde13c49100ea9f4190a2308d
Johnson.ai <jai@mac.home>
2026-09-20 22:23:00 -0400

    Remove unused legacy scripts, graphics libraries, and images

--- js/index.js
+++
@@ -1,7 +0,0 @@
-/* Shoe Portfolio*/
-function showLoadingAnimation(event) {
-    event.preventDefault();
-  
-    // Open the loading animation in a new tab or window
-    window.open(event.target.href, '_blank');
-  }
